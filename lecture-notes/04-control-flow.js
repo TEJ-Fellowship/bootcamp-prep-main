0b29a2d2f5efdea56a525eb9de3117c59e238a23
rushilshakya <rushil.shakya@gmail.com>
2022-06-02 19:19:27 +0545

    feat: Add note on control flow

--- lecture-notes/04-control-flow.js
+++ lecture-notes/04-control-flow.js
@@ -6,3 +6,18 @@
 // else if / else
 // else if
 // logical operator
+
+let word = "perfect";
+// when we call a method, such as toUpperCase()
+// on any data type, such as the string variable word
+let lastWord = word.toUpperCase();
+
+// the data itself might or might not change
+// in the case of the method toUpperCase() on a string
+// the data itself doesn't change
+// it only evaluates to the changed state
+console.log(word);
+
+// so you have to capture the value in a different variable
+// if you want to save the evaluated state
+console.log(lastWord);
