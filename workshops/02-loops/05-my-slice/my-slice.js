3d3e74040e5c449c7b1ed7d493d4add7c97749cd
rushilshakya <rushil.shakya@gmail.com>
2022-06-03 16:37:52 +0545

    feat: Add solutions to loop

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function mySlice(originalString, startIdx = 0, endIdx = originalString.length) {
+  let stri = "";
+  for (let i = startIdx; i < endIdx; i++) {
+    stri += originalString[i];
+  }
+  return stri;
+
+  //   if (originalString && startIdx && endIdx) {
+  //     for (let i = startIdx; i < endIdx; i++) {
+  //       stri += originalString[i];
+  //     }
+  //     return stri;
+  //   } else if (originalString && startIdx) {
+  //     for (let i = startIdx; i < originalString.length; i++) {
+  //       stri += originalString[i];
+  //     }
+  //     return stri;
+  //   } else {
+  //     return originalString;
+  //   }
+}
