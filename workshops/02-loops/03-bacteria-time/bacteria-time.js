1169d9bcd7ed38f7e36426e44c0565755454bf17
rushilshakya <rushil.shakya@gmail.com>
2022-06-04 08:20:41 +0545

    feat: Solve bacteria time

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function bacteriaTime(curr, target) {
+  if (target < curr) return "targetNum must be larger than currentNum";
+  let time = 0;
+  for (let i = curr; i < target; i += i) {
+    time += 20;
+  }
+  return time;
+}
