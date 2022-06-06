c974dd0c852ded8c474f408a6a8e5e98d91ae6ac
rushilshakya <rushil.shakya@gmail.com>
2022-06-06 10:43:19 +0545

    feat: notes of coercion

--- lecture-notes/08-coercion.js
+++ lecture-notes/08-coercion.js
@@ -1,38 +1,39 @@
 let numSum = "10" + 20;
-console.log(numSum);
+console.log("'10' + 20 is", numSum);
+console.log(`${numSum} typeof is`, typeof numSum);
 
 let str = "niru";
 
 if (str) {
-  console.log("this is true");
+  console.log("The value of variable str is true");
 } else {
-  console.log("this is false");
+  console.log("The value of variable str is false");
 }
 
 // Testing if using division on string will result in NaN
 let isItNAN = 1 / str;
-console.log(isItNAN);
+console.log(`1 / ${str} is`, isItNAN);
 
 // Testing if coercing a string to number will result in NaN
 // let newStr1 = "hello there";
 let newStr1 = "123ab456";
 let numStr = Number(newStr1);
 // coercing string to number does result in NaN
-console.log(numStr);
+console.log(`Number(${numStr}) is`, numStr);
 
-console.log(typeof newStr1);
+console.log(`typeof ${newStr1} is`, typeof newStr1);
 //data type of NaN is 'number'
-console.log(typeof numStr);
+console.log(`typeof ${numStr} is`, typeof numStr);
 
 // "true" double equals true is not true!!
 // this is why you should not use == double equals
 // 1st example of "true" is not equal to true
-let notSure = true == "true";
-console.log(notSure);
+let notSure = false == "false";
+console.log(`false == "false" evaluates to`, notSure);
 
 // 2nd example of "true" is not equal to true
 if ("true" == true) {
-  console.log("true is true");
+  console.log("true == 'true' is true");
 } else {
-  console.log("true is false");
+  console.log("true == 'true' is false");
 }
