1f8bb62d79e5905e7d2787bf2f69284008d85769
rushilshakya <rushil.shakya@gmail.com>
2022-06-06 10:42:44 +0545

    feat: solve zero-dark-thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(num) {
+  //   "102302"
+  let strName = String(num);
+  let result = "";
+
+  for (let i = 0; i < strName.length; i++) {
+    //"0"
+    if (strName[i] !== "0") {
+      result = result + strName[i];
+    }
+  }
+
+  return Number(result);
+}
+
+let ret = zeroDarkThirty(102302); // => 1232
+
+console.log(ret);
+console.log(typeof ret);
