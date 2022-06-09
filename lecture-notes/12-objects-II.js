386602b324c8e7170f17a9721b415341ab211d08
rushilshakya <rushil.shakya@gmail.com>
2022-06-09 16:31:49 +0545

    feat: update objects II notes

--- lecture-notes/12-objects-II.js
+++ lecture-notes/12-objects-II.js
@@ -2,7 +2,6 @@
 OBJECTS:
 - look at existing methods on objects we have used before
   - e.g. array.push
-- write your own simple method
 - write your own method with parameter
 - write your own method with this
 */
