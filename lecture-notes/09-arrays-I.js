004458c23ab0cdb220d4911779af32d18417fd1b
rushilshakya <rushil.shakya@gmail.com>
2022-06-10 14:53:16 +0545

    feat: edit arrays i lecture notes

--- lecture-notes/09-arrays-I.js
+++ lecture-notes/09-arrays-I.js
@@ -18,6 +18,19 @@ ARRAYS I
       - slice(1,2), slice(1), slice(): copy
 */
 
-let names = ["a", "b", "c"];
+let names = ["a", "b", "c", 1, 2, 3];
 
-console.log(names.slice(1, 1));
+console.log(names.push(3));
+console.log(names);
+console.log(names.pop(2));
+console.log(names);
+console.log(names.reverse());
+console.log(names);
+console.log(names.splice());
+console.log(names);
+console.log(names.splice(2, 1, 4, 4));
+console.log(names);
+
+names = [6, "hey", "there", 2];
+console.log(names.splice(3, 0, "you"));
+console.log(names);
