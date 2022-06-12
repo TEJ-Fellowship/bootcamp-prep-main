42c7728dca3d5c7254338b89ed4b8689454b792f
rushilshakya <rushil.shakya@gmail.com>
2022-06-12 22:34:24 +0545

    feat: add notes for pbv pbr

--- lecture-notes/14-pbv-pbr.js
+++ lecture-notes/14-pbv-pbr.js
@@ -2,10 +2,14 @@
 - declaring primitives
 - assigning one primitive var to another
 - equality for primitive
+- operating on primitive
 - passing primitives into function
 
 - declaring collection / complex
 - assigning one collection to another
 - equality for collection
+    - assigned
+    - separately declared
+- operating on primitive
 - passing collections into function
 */
