2390efd2830a4e98ae4bb0c301e6dd90b8119919
rushilshakya <rushil.shakya@gmail.com>
2022-06-12 22:32:30 +0545

    feat: add pbv pbr lecture notes

---
+++ lecture-notes/14-pbv-pbr.js
@@ -0,0 +1,11 @@
+/*
+- declaring primitives
+- assigning one primitive var to another
+- equality for primitive
+- passing primitives into function
+
+- declaring collection / complex
+- assigning one collection to another
+- equality for collection
+- passing collections into function
+*/
