7e2bea8a7a74120475f3d22c20f5d8780cc63d4e
rushilshakya <rushil.shakya@gmail.com>
2022-06-13 21:36:43 +0545

    feat: solve biller builder from higher order ii

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function billerBuilder(state) {
+  function retBiller(amount) {
+    if (state === "NY") {
+      return amount * 1.03 * 1.04;
+    } else {
+      return amount * 1.05 * 1.06625;
+    }
+  }
+
+  return retBiller;
+}
