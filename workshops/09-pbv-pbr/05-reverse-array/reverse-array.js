07791605a346dbd4c13498f02c5027353052f35d
rushilshakya <rushil.shakya@gmail.com>
2022-06-13 20:54:53 +0545

    feat: solve all pbr problems

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function reverseArray(arr) {
+  //declare variable to capture reversed array
+  let rev = [];
+
+  //lets go from reverse of our input
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // and push the reversed order value into rev
+    rev.push(arr[i]);
+  }
+
+  //now, lets loop through rev
+  for (let i = 0; i < rev.length; i++) {
+    //and assign the values from rev to input array
+    arr[i] = rev[i];
+  }
+
+  return arr;
+}
--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+function deeperCopy(arr) {
+  //declare variable to store the copy
+  let retArr = [];
+  //looping through the input array
+  for (let i = 0; i < arr.length; i++) {
+    let currVal = arr[i];
+    //if the current element is not an array
+    if (!Array.isArray(currVal)) {
+      //then just push it into the copy array
+      retArr.push(currVal);
+    } else {
+      //if the element is an array, then create new array to put it in
+      let innerArray = [];
+      //loop through the inner array
+      for (let j = 0; j < currVal.length; j++) {
+        //push the values into the new array
+        innerArray.push(currVal[j]);
+      }
+      //then push this new array into our copy array
+      retArr.push(innerArray);
+    }
+  }
+
+  return retArr;
+}
