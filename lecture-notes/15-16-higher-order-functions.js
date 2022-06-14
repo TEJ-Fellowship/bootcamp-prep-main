f1ee416324e9092c431fee3ae4905d41159e7f1f
rushilshakya <rushil.shakya@gmail.com>
2022-06-14 22:22:21 +0545

    feat: add lecture notes for higher order functions

---
+++ lecture-notes/15-16-higher-order-functions.js
@@ -0,0 +1,14 @@
+/*
+- Functions can be put wherever other data types can be put
+    - Var, array, object, pass as argument, return 
+- Passing function & arg to function, that will call it
+    - Writing your own forEach
+        - Array of funcs, one val
+        - Array of Vals, one function
+    - .forEach function
+        - forEach does not return anything
+
+- Function returning function
+    - Number of times function called counter
+    - Multiplication builder
+*/
